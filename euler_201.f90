b5400dc361c75639c3038f4b923e114337fc1df6
Kyle Kanos <kylekanos@yahoo.com>
2016-05-13 06:01:15 -0400

    Create euler_201.f90
    My solution but redacted some of the key elements since the more difficult problems should not be discussed so openly

---
+++ euler_201.f90
@@ -0,0 +1,65 @@
+program euler_201
+   use EulerCommon
+   implicit none
+   integer(int64), parameter :: kmax=100
+   integer(int64), dimension(kmax) :: sq
+   integer(int64) :: i, k, tot, ctr=0, one=1, zero=0
+   integer(int64) :: max_sum
+   type dictionary
+      integer(int64) :: key,val
+   end type dictionary
+   type(dictionary), allocatable :: sums(:)
+   real(dp) :: stt, fin
+   call cpu_time(stt)
+
+   ! set squares
+   do i=1,kmax
+      sq(i) = i*i
+   end do !- i
+   max_sum = sum(sq(:50))*1.06
+
+   ! set the sums
+   allocate(sums(1))
+   sums(1)%key = 0
+   sums(1)%val = 0
+
+   ctr = zero
+
+   call explore(one,zero,zero)
+
+   tot = 0
+   do i=1,size(sums)
+      if (sums(i)%val == one) tot = tot + one
+   end do !- i
+   call cpu_time(fin)
+   print *,tot*sum(sq)
+   print '("took ",f0.6," seconds")',fin-stt
+
+contains
+   recursive subroutine explore(start, curr_sum, lev)
+      integer(int64), intent(in) :: start, curr_sum, lev
+      integer(int64) :: m, p_ct, m1, cm, l1
+      
+      ! step 1: check level
+      if (lev == ???) then
+         call ???
+         ctr = ctr + one
+         return
+      end if
+
+      ! explore somre more
+      do m=start,kmax
+         cm = curr_sum + m*m
+         if (cm > max_sum) return
+
+         p_ct = ctr
+         m1 = m + one
+         l1 = lev + one
+         call explore(m1, cm, l1)
+         if (???) exit
+      end do !- m
+   end subroutine explore
+
+! redacted two procedures since solutions should not be full for some of the more difficult ones....
+end program euler_201
+
