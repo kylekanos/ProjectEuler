40f29f760a5b88b5b2e66ec02a7f82fd71346df5
Kyle Kanos <kylekanos@yahoo.com>
2016-05-27 10:28:53 -0400

    Update euler_169.f90

--- euler_169.f90
+++ euler_169.f90
@@ -7,15 +7,17 @@ program euler_169
     
     a = 1
     b = 0
-    do i=1,84
+    i = 0
+    do
        if (mod(twos(26),2)==1) then
           b = a + b
        else
           a = a + b
        endif
        call divide_by_two(twos)
-       !i = i + 1
+       i = i + 1
        print '(3(i0,2x),26(i0))',i,a,b,twos
+       if (sum(twos)==0) exit
     end do 
     print '("The hyperbinary representation of 10**25 = ",i0)',b
 
@@ -24,9 +26,11 @@ contains
       integer, dimension(26), intent(inout) :: twos
       integer :: i, tmp(26)
       tmp = 0
+      ! do first round of integer division
       do i=1,26
          tmp(i) = twos(i)/2
       end do !- i
+      ! add "0.5" term to odd terms
       do i=2,26
          if (mod(twos(i-1),2)==1 .and. twos(i-1)/=0) then
             tmp(i) = tmp(i) + 5
